70d14c79ec75430e0c5ffae0dca88f9db9565f29
viteo <simonenko@rtp>
2021-09-24 10:45:30 +0300

    SDO client added

--- device/co_driver/CO_driver_blank.c
+++ device/co_driver/CO_driver_blank.c
@@ -215,7 +215,7 @@ CO_ReturnError_t CO_CANsend(CO_CANmodule_t *CANmodule, CO_CANtx_t *buffer)
 		txMsg.StdId = buffer->ident;
 		txMsg.DLC = buffer->DLC;
 		array_copy_8(txMsg.Data, buffer->data, txMsg.DLC);
-		CAN_Transmit(CAN1, &txMsg);
+		CAN_Transmit(CANmodule->CANptr, &txMsg);
 	}
 	/* if no buffer is free, message will be sent by interrupt */
 	else
@@ -436,4 +436,91 @@ void CO_CANinterruptTx(CO_CANmodule_t *CANmodule)
 			CANmodule->CANtxCount = 0U;
 		}
 	}
+}
+
+#include "301/CO_SDOclient.h"
+CO_SDO_abortCode_t read_SDO(CO_SDOclient_t *SDO_C, uint8_t nodeId, uint16_t index, uint8_t subIndex, uint8_t *buf, size_t bufSize, size_t *readSize)
+{
+	CO_SDO_return_t SDO_ret;
+	CO_SDO_abortCode_t abortCode = CO_SDO_AB_NONE;
+
+	// setup client (this can be skipped, if remote device don't change)
+	SDO_ret = CO_SDOclient_setup(SDO_C, CO_CAN_ID_SDO_CLI + nodeId, CO_CAN_ID_SDO_SRV + nodeId, nodeId);
+	if (SDO_ret != CO_SDO_RT_ok_communicationEnd)
+	{
+		abortCode = CO_SDO_AB_GENERAL;
+	}
+
+	// initiate upload
+	SDO_ret = CO_SDOclientUploadInitiate(SDO_C, index, subIndex, 1000, false);
+	if (SDO_ret != CO_SDO_RT_ok_communicationEnd)
+	{
+		abortCode = CO_SDO_AB_GENERAL;
+	}
+
+	// upload data
+	do
+	{
+		uint32_t timeDifference_us = 1000;
+
+		SDO_ret = CO_SDOclientUpload(SDO_C, timeDifference_us, false, &abortCode, NULL, NULL, NULL);
+		if (SDO_ret < 0)
+		{
+			break;
+		}
+
+		DWT_Delay_us(timeDifference_us); //todo is it needed?
+	} while (SDO_ret > 0);
+
+	// copy data to the user buffer (for long data function must be called
+	// several times inside the loop)
+	*readSize = CO_SDOclientUploadBufRead(SDO_C, buf, bufSize);
+
+//	return CO_SDO_AB_NONE;
+	return abortCode;
+}
+
+CO_SDO_abortCode_t write_SDO(CO_SDOclient_t *SDO_C, uint8_t nodeId, uint16_t index, uint8_t subIndex, uint8_t *data, size_t dataSize)
+{
+	CO_SDO_return_t SDO_ret;
+	bool_t bufferPartial = false;
+
+	// setup client (this can be skipped, if remote device is the same)
+	SDO_ret = CO_SDOclient_setup(SDO_C, CO_CAN_ID_SDO_CLI + nodeId, CO_CAN_ID_SDO_SRV + nodeId, nodeId);
+	if (SDO_ret != CO_SDO_RT_ok_communicationEnd)
+	{
+		return -1;
+	}
+
+	// initiate download
+	SDO_ret = CO_SDOclientDownloadInitiate(SDO_C, index, subIndex, dataSize, 1000, false);
+	if (SDO_ret != CO_SDO_RT_ok_communicationEnd)
+	{
+		return -1;
+	}
+
+	// fill data
+	size_t nWritten = CO_SDOclientDownloadBufWrite(SDO_C, data, dataSize);
+	if (nWritten < dataSize)
+	{
+		bufferPartial = true;
+		// If SDO Fifo buffer is too small, data can be refilled in the loop.
+	}
+
+	//download data
+	do
+	{
+		uint32_t timeDifference_us = 1000;
+		CO_SDO_abortCode_t abortCode = CO_SDO_AB_NONE;
+
+		SDO_ret = CO_SDOclientDownload(SDO_C, timeDifference_us, false, bufferPartial, &abortCode, NULL, NULL);
+		if (SDO_ret < 0)
+		{
+			return abortCode;
+		}
+
+		DWT_Delay_us(timeDifference_us); //todo is it needed?
+	} while (SDO_ret > 0);
+
+	return CO_SDO_AB_NONE;
 }
